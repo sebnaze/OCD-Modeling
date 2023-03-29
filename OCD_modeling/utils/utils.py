55cb5a01b5642dcbc38d9f34ac527ceb3755a2a0
sNaze <sebastien.naze@gmail.com>
2023-03-29 16:41:45 +1000

    added neurosrv01 in list of hosts

--- OCD_modeling/utils/utils.py
+++ OCD_modeling/utils/utils.py
@@ -6,9 +6,11 @@ def get_working_dir():
     """ get computer name to set working path """
     if platform.node()=='qimr18844':
         working_dir = '/home/sebastin/working/'
+    elif platform.node()=='neurosrv01':
+        working_dir = '/home/sebastienn/working/'
     elif 'hpcnode' in platform.node():
         working_dir = '/mnt/lustre/working/'
     else:
-        print('Computer unknown! Setting working dir as /working')
+        print('Mince! Computer unknown! Setting working dir as /working')
         working_dir = '/working/'
     return working_dir
