a9ebbd82823c6e365c83a053ee02f419c6b60f72
sNaze <sebastien.naze@gmail.com>
2023-04-05 15:28:27 +1000

    added the Ornstein-Ulhenbeck parameters to the ABC optimization

--- OCD_modeling/mcmc/abc.py
+++ OCD_modeling/mcmc/abc.py
@@ -5,6 +5,7 @@
 #
 # QIMR Berghofer 2023
 
+import argparse
 from datetime import datetime
 import importlib
 import inspect
@@ -15,8 +16,9 @@ import pandas as pd
 import pyabc
 import time
 
+import OCD_modeling
 from OCD_modeling.models import ReducedWongWang as RWW
-from OCD_modeling.utils.utils import get_working_dir
+from OCD_modeling.hpc import parallel_launcher
 
 
 
@@ -25,32 +27,46 @@ default_model_params = dict()
 default_model_params['I_0']       = 0.3
 default_model_params['J_N']       = 0.2609
 default_model_params['w']         = 0.9
-default_model_params['G']         = 2.
+default_model_params['G']         = 2.5
 default_model_params['tau_S']     = 100.
 default_model_params['gamma']     = 0.000641
 default_model_params['sigma']     = 0.1
 default_model_params['N']         = 4
 default_model_params['dt']        = 0.01
 default_model_params['C']         = np.zeros((default_model_params['N'], default_model_params['N']))
+default_model_params['sigma_C']   = np.zeros((default_model_params['N'], default_model_params['N']))
+default_model_params['eta_C']     = np.zeros((default_model_params['N'], default_model_params['N']))
 
 # simulation default parameters
 default_sim_params = dict()
-default_sim_params['t_tot']         = 1000
+default_sim_params['t_tot']         = 8000
+default_sim_params['rec_vars']      = ['C_13', 'C_24']
 default_sim_params['sf']            = 100
-default_sim_params['t_rec']         = [100,1000]
+
+# BOLD default parameters
+default_bold_params = dict()
+default_bold_params['t_range']      = [5000,8000]
+default_bold_params['transient']    = 60
 
 # PRIORS
 # lower_bound, upper_bound
-sigma_min, sigma_max    = 0.01, 0.5     # noise amplitude
-G_min, G_max            = 1, 3          # global coupling
-C_12_min, C_12_max      = -1, 1         # PFC -> OFC
-C_21_min, C_21_max      = -1, 1         # OFC -> PFC
-C_13_min, C_13_max      = -1, 1         # NAcc -> OFC
-C_31_min, C_31_max      = 0, 1          # OFC -> NAcc
-C_24_min, C_24_max      = -1, 1         # Put -> PFC
-C_42_min, C_42_max      = 0, 1          # PFC -> Put
-C_34_min, C_34_max      = -1, 0         # Put -> NAcc
-C_43_min, C_43_max      = -1, 0
+sigma_min, sigma_max    = 0.001, 0.1     # noise amplitude
+G_min, G_max            = 1, 3           # global coupling
+C_12_min, C_12_max      = -0.5, 0.5      # PFC -> OFC
+C_21_min, C_21_max      = -0.5, 0.5      # OFC -> PFC
+C_13_min, C_13_max      = -0.5, 0.5      # NAcc -> OFC
+C_31_min, C_31_max      = 0, 0.5         # OFC -> NAcc
+C_24_min, C_24_max      = -0.5, 0.5      # Put -> PFC
+C_42_min, C_42_max      = 0, 0.5         # PFC -> Put
+C_34_min, C_34_max      = -0.5, 0.5      # Put -> NAcc
+C_43_min, C_43_max      = -0.5, 0.5      # Nacc -> Put
+
+# coupling noises on Ornstein-Uhlenbeck process 
+sigma_C_13_min, sigma_C_13_max  = 0, 0.5
+sigma_C_24_min, sigma_C_24_max  = 0, 0.5
+eta_C_13_min, eta_C_13_max  = 0, 0.1
+eta_C_24_min, eta_C_24_max  = 0, 0.1
+
 
 prior = pyabc.Distribution(
     sigma = pyabc.RV("uniform", sigma_min, sigma_max - sigma_min),
@@ -63,10 +79,15 @@ prior = pyabc.Distribution(
     C_42 = pyabc.RV("uniform", C_42_min, C_42_max - C_42_min),
     C_34 = pyabc.RV("uniform", C_34_min, C_34_max - C_34_min),
     C_43 = pyabc.RV("uniform", C_43_min, C_43_max - C_43_min),
+    sigma_C_13 = pyabc.RV("uniform", sigma_C_13_min, sigma_C_13_max - sigma_C_13_min),
+    sigma_C_24 = pyabc.RV("uniform", sigma_C_24_min, sigma_C_24_max - sigma_C_24_min),
+    eta_C_13 = pyabc.RV("uniform", eta_C_13_min, eta_C_13_max - eta_C_13_min),
+    eta_C_24 = pyabc.RV("uniform", eta_C_24_min, eta_C_24_max - eta_C_24_min)
 )
 
 def get_default_args(func):
     """ https://stackoverflow.com/questions/12627118/get-a-function-arguments-default-value """
+    """ (actually not in use -- deprecated) """ 
     signature = inspect.signature(func)
     return {
         k: v.default
@@ -77,25 +98,34 @@ def get_default_args(func):
 def unpack_params(in_params):
     """ unpack parameters to be given to model and simulation """
     model_params = default_model_params
-    #sim_params = get_default_args(RWW.ReducedWongWangND().run)
     sim_params = default_sim_params
+    bold_params = default_bold_params
+    control_params = dict()  # if keept empty, unused 
+
+    # unpack model's params
     for k,v in in_params.items():
         if k in default_model_params.keys():
             model_params[k] = v
+        elif ('C_' in k):
+            var, inds = k.rsplit('_', maxsplit=1)
+            j,i = int(inds[0])-1, int(inds[1])-1
+            model_params[var][j,i] = v
+
+        # unpack sim params
         elif k in default_sim_params.keys():
             sim_params[k] = v
-        elif k.startswith('C_'):
-                inds = k.split('_')[1]
-                j,i = int(inds[0])-1, int(inds[1])-1
-                model_params['C'][j,i] = v
+        
+        # unpack bold params
+        elif k in default_bold_params.keys():
+            bold_params[k] = v
         else:
             print('parameter {} is unknown, it is being discarded.'.format(k))
             continue
-    return model_params, sim_params
+    return model_params, sim_params, control_params, bold_params
 
 def simulate_rww(params):
     """ instanciate model and simulate trace """
-    model_params, sim_params = unpack_params(params)
+    model_params, sim_params, control_params, bold_params = unpack_params(params)
     #print(model_params)
     rww = RWW.ReducedWongWangND(**model_params)
     rww.run(**sim_params)
@@ -103,6 +133,21 @@ def simulate_rww(params):
     RWW.compute_bold(rww)
     r, corr_diff, corr = RWW.score_model(rww, coh='pat')
     return {'r': r, 'corr_diff':corr_diff, 'corr':corr}
+
+
+def simulate_population_rww(params):
+    """ instanciate models and simulate traces """
+    args = argparse.Namespace()
+    args.n_sims = 50
+    args.n_jobs = 12
+    args.model_pars, args.sim_pars, args.control_pars, args.bold_pars = unpack_params(params)
+    
+    #sim_objs = parallel_launcher.launch_simulations(args)
+    sim_objs = parallel_launcher.launch_pool_simulations(args)
+    
+    RMSE = RWW.score_population_models(sim_objs, cohort='patients')
+    return {'RMSE': RMSE}
+
 
 
 def evaluate_prediction(history, n_samples=1000):
@@ -134,14 +179,15 @@ def sim_output_to_df(sim_output, coh='con'):
 
 if __name__ == '__main__':
     today = datetime.now().strftime("%Y%m%d")
-    trace_name = 'rww4D_'+today
-    working_dir = get_working_dir()
+    trace_name = 'rww4D_OU_'+today
+    working_dir = OCD_modeling.utils.utils.get_working_dir() 
     proj_dir = os.path.join(working_dir, 'lab_lucac/sebastiN/projects/OCD_modeling')
-    sampler = pyabc.sampler.MulticoreEvalParallelSampler(n_procs=32)
-    abc = pyabc.ABCSMC(simulate_rww, prior, RWW.distance, sampler=sampler, population_size=1000, max_nr_recorded_particles=1000000)
+    #sampler = pyabc.sampler.MulticoreEvalParallelSampler(n_procs=1)
+    sampler = pyabc.sampler.RedisEvalParallelSampler(host="10.10.51.21", port=6379, password='bayesopt1234321')
+    sampler.daemon = False
+    abc = pyabc.ABCSMC(simulate_population_rww, prior, RWW.distance, sampler=sampler, population_size=100, max_nr_recorded_particles=1000)
     abc_id = abc.new(
         "sqlite:///" + os.path.join(proj_dir, 'traces', trace_name+".db"),
-        {"r": 1, 'corr_diff':0, 'corr':np.zeros((default_model_params['N'],default_model_params['N']))}  # observation
-        # note: observation here is dummy, the distane function does not use it.
+        {"RMSE": 0}  # observation # note: here is dummy, the distance function does not use it.
     )
-    history = abc.run(max_nr_populations=10, minimum_epsilon=0.1)
+    history = abc.run(max_nr_populations=10, minimum_epsilon=0.001)
