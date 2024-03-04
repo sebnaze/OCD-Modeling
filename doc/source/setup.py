9e13e28a1e3ea6fd51a9135b0c4eb073c39d3da1
sNaze <sebastien.naze@gmail.com>
2024-03-05 06:05:57 +1000

    setup.py in docs..

---
+++ doc/source/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='OCD_modeling',
+      version='1.0',
+      description='Modeling frontostriatal mechanisms and simulating intervention targets restoring \
+      ventro-dorsal dynamics in obsessive-compulsive disorder.',
+      author='Sebastien Naze',
+      author_email='sebastien.naze@gmail.com',
+      url='www.github.com',
+      packages=find_packages(),
+     )
