4ec00ffe3b32ca6382de03f7c22c56b246993a5b
sNaze <sebastien.naze@gmail.com>
2024-03-07 02:24:42 +1000

    setup

--- setup.py
+++ setup.py
@@ -3,10 +3,26 @@
 from setuptools import setup, find_packages
 
 setup(name='OCD_modeling',
-      version='0.2',
-      description='Modeling OCD disruptions projects',
+      version='1.0',
+      description='Modeling frontostriatal mechanisms and simulating intervention targets restoring \
+      ventro-dorsal dynamics in obsessive-compulsive disorder.',
       author='Sebastien Naze',
       author_email='sebastien.naze@gmail.com',
-      url='www.github.com',
+      url='https://github.com/sebnaze/OCD-Modeling',
       packages=find_packages(),
+      install_requires=['dill',
+                        'nilearn', 
+                        'numpy', 
+                        'pandas',
+                        'pingouin',
+                        'pickle',
+                        'pyabc',
+                        'PyDSTool',
+                        'pymeshfix',
+                        'pyvista',
+                        'scipy', 
+                        'seaborn',
+                        'sklearn',
+                        'sympy'
+                        ]
      )
